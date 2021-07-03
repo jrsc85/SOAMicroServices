09d9d587f6892cfbf407253917a7246249f83ed3
Desarrollo <alanff2800@gmail.com>
2021-07-02 20:19:56 -0500

    Agenda
    Falta el de datos medicos

--- agendaMedica/server.js
+++ agendaMedica/server.js
@@ -17,9 +17,29 @@ app.use(cors());
 app.use('/medic', medicService);
 
 app.get("/", function(req, res){
-        res.send({"stage": "dev"})
-})
+        res.send({"stage": "dev"});
+});
 
+app.get("getAll", function(req, res){
+    var params = {
+         //Nombre de la tabla a la que hará referencia
+         TableName: "medic_schedule_a"
+    };
+
+    dynamodb.scan(params, function(err, response){
+        if(err){
+            res.status(500).send(error);
+        }else{
+            res.status(200).send(response);
+        }
+    });
+});
+
+
+/**************************************************************************/
+//-------------------------------PACIENTE------------------------------------
+
+//Crear paciente
 app.post("/createPacient", jsonParser, function(req, res){
     let id =  uuid4();
     let date = new Date().toLocaleString('es-MX', {timeZone: 'America/Mexico_City'});
@@ -28,11 +48,19 @@ app.post("/createPacient", jsonParser, function(req, res){
         try{
             let data = {
                 "pk": "Patient",
-                "sk": id,
-                "username": req.body.username,
-                "lastname": req.body.lastname,
-                "emai": req.body.email,
-                "createdAt": date
+                "sk": "Patient-" + id,
+                "nombres": req.body.nombres,
+                "apellido_P": req.body.apellido_P,
+                "apellido_M": req.body.apellido_M,
+                "genero": req.body.genero,
+                "fechaNacimiento": req.body.fechaNacimiento,
+                "estado": req.body.estado,
+                "ciudad": req.body.ciudad,
+                "telefono": req.body.telefono,
+                "email": req.body.email,
+                "password": req.body.password,
+                "createdAt": date,
+                "changedAt": date
             };
 
             var params = {
@@ -57,45 +85,24 @@ app.post("/createPacient", jsonParser, function(req, res){
     })();
 });
 
-app.get("getAll", function(req, res){
-    var params = {
-         //Nombre de la tabla a la que hará referencia
-         TableName: "medic_schedule_a"
-    };
-
-    dynamodb.scan(params, function(err, response){
-        if(err){
-            res.status(500).send(error);
-        }else{
-            res.status(200).send(response);
-        }
-    });
-});
-
-
-app.get("/getPacient/:name", function(req, res){
+//Obtener datos del paciente por medio de su SK
+app.get("/getPacient/:sk", function(req, res){
     (async ()=> {
         try{
             var params = {
                  //Nombre de la tabla a la que hará referencia
                 TableName: "medic_schedule_a",
-                KeyConditionExpression: "pk = :pk",
+                KeyConditionExpression: "pk = :pk AND sk = :sk",
                 ExpressionAttributeValues: {
                     ":pk": "Patient",
-                    ":username": req.params.username
-                },
-                ExpressionAttributeNames:{
-                    "#username": "username"
-                },
-                /*#username representa la columna de la base de datos
-                  :username representa lo que mandamos */
-
-                FilterExpression: "#username = :username"
+                    ":sk":req.params.sk
+                }
             };
 
             dynamodb.query(params, function(err, response){
                 if(err){
                     res.status(500).send(err);
+                    console.log(req.params);
                 }else{
                     res.status(200).send(response.Items);
                 }
@@ -107,39 +114,60 @@ app.get("/getPacient/:name", function(req, res){
     })();
 });
 
-
+//Actualizar datos del paciente, menos su contraseña
 app.put("/updatePatient/:sk", jsonParser, function(req, res){
+    let date = new Date().toLocaleString('es-MX', {timeZone: 'America/Mexico_City'});
+
     var params = {
          //Nombre de la tabla a la que hará referencia
-        TableName: "school_a", 
+        TableName: "medic_schedule_a", 
         Key: {
             "pk": "Patient",
-            "sk": req.param.sk
+            "sk": req.params.sk
         },
-        UpdateExpression: "set lastname = :l, email = :e",
+        UpdateExpression: "set nombres = :n,"+
+            "apellido_P = :apP," + 
+            "apellido_M = :apM," + 
+            "genero = :g," +
+            "fechaNacimiento = :fn," + 
+            "estado = :est," +
+            "ciudad = :c," +
+            "telefono = :tel," +
+            "email = :e," +
+            "changedAt = :chang",
         ExpressionAttributeValues: {
-            ":l": req.body.lastname,
-            ":e": req.body.email
+            ":n": req.body.nombres,
+            ":apP": req.body.apellido_P,
+            ":apM": req.body.apellido_M,
+            ":e": req.body.email,
+            ":tel": req.body.telefono,
+            ":g": req.body.genero,
+            ":fn": req.body.fechaNacimiento,
+            ":est": req.body.estado,
+            ":c": req.body.ciudad,
+            ":chang": date
         },
-        ReturnValues: "UPDATE_NEW"
+        ReturnValues: "UPDATED_NEW"
     };
 
     dynamodb.update(params, function(err, response){
         if(err){
             res.status(500).send(err);
+            console.log(req.body);
         }else{
             res.status(200).send(response);
         }
     });
 });
 
+//Borrar paciente -HAY QUE CONSIDERAR SI LO BORRAMOS O HACER UN "STATUS: INNACTIVE"-
 app.delete("/deletePatient/:sk", function(req, res){
     var params = {
         //Nombre de la tabla a la que hará referencia
         TableName: "medic_schedule_a", 
         Key: {
             "pk": "Patient",
-            "sk": req.param.sk
+            "sk": req.params.sk
         }
     };
 
@@ -154,7 +182,6 @@ app.delete("/deletePatient/:sk", function(req, res){
 
 /**************************************************/
 //---------------------MEDICO-----------------------
-
 //Crear nuevo doctor
 app.post("/createDoctor", jsonParser, function(req, res){
     let id =  uuid4();
@@ -164,10 +191,10 @@ app.post("/createDoctor", jsonParser, function(req, res){
         try{
             let data = {
                 "pk": "Doctor",
-                "sk": id,
-                "nombres": req.body.user_names,
-                "apellido_P": req.body.apellidoP,
-                "apellido_M": req.body.apellidoM,
+                "sk": "Doctor-" + id,
+                "nombres": req.body.nombres,
+                "apellido_P": req.body.apellido_P,
+                "apellido_M": req.body.apellido_M,
                 "genero": req.body.genero,
                 "fechaNacimiento": req.body.fechaNacimiento,
                 "estado": req.body.estado,
@@ -200,24 +227,18 @@ app.post("/createDoctor", jsonParser, function(req, res){
     })();
 });
 
-//Obtener datos del dotor por medio de su sk
-app.get("/medicInfo/:sk", function(req, res){
+//Obtener datos del doctor por medio de su SK
+app.get("/getDoctor/:sk", function(req, res){
     (async ()=> {
         try{
             var params = {
                  //Nombre de la tabla a la que hará referencia
                 TableName: "medic_schedule_a",
-                KeyConditionExpression: "pk = :pk",
+                KeyConditionExpression: "pk = :pk AND sk = :sk",
                 ExpressionAttributeValues: {
                     ":pk": "Doctor",
                     ":sk": req.params.sk
-                },
-                ExpressionAttributeNames:{
-                    "#sk": "sk"
-                },
-                /*#sk representa la columna de la base de datos
-                  :sk representa lo que mandamos */
-                FilterExpression: "#sk = :sk"
+                }
             };
 
             dynamodb.query(params, function(err, response){
@@ -236,33 +257,35 @@ app.get("/medicInfo/:sk", function(req, res){
 
 //Actualizar datos
 app.put("/update/DoctorData/:sk", jsonParser, function(req, res){
+    let date = new Date().toLocaleString('es-MX', {timeZone: 'America/Mexico_City'});
     var params = {
          //Nombre de la tabla a la que hará referencia
-        TableName: "school_a", 
+        TableName: "medic_schedule_a", 
         Key: {
-            "pk": "Patient",
-            "sk": req.param.sk
+            "pk": "Doctor",
+            "sk": req.params.sk
         },
         UpdateExpression: "set nombres = :n,"+
-            "apellido_P = :apP," + 
-            "apellido_M = apM," + 
-            "email = :e" + 
-            "genero = :g" +
-            "fechaNacimiento = :fn" + 
-            "estado = :est" +
-            "ciudad = :c",
+            " apellido_P = :apP," + 
+            " apellido_M = :apM," + 
+            " email = :e," + 
+            " genero = :g," +
+            " fechaNacimiento = :fn," + 
+            " estado = :est," +
+            " ciudad = :c," +
+            " changedAt = :chang",
         ExpressionAttributeValues: {
-            ":n": req.body.user_names,
-            ":apP": req.body.apellidoP,
-            ":apM": req.body.apellidoM,
+            ":n": req.body.nombres,
+            ":apP": req.body.apellido_P,
+            ":apM": req.body.apellido_M,
             ":e": req.body.email,
             ":g": req.body.genero,
             ":fn": req.body.fechaNacimiento,
             ":est": req.body.estado,
             ":c": req.body.ciudad,
-            "changedAt": date
+            ":chang": date
         },
-        ReturnValues: "UPDATE_NEW"
+        ReturnValues: "UPDATED_NEW"
     };
 
     dynamodb.update(params, function(err, response){
@@ -281,7 +304,7 @@ app.delete("/deleteDoctor/:sk", function(req, res){
         TableName: "medic_schedule_a", 
         Key: {
             "pk": "Doctor",
-            "sk": req.param.sk
+            "sk": req.params.sk
         }
     };
 
@@ -295,6 +318,213 @@ app.delete("/deleteDoctor/:sk", function(req, res){
 });
 
 /******************************************************/
+//------------------------AGENDA------------------------
+
+//Crear cita
+app.post("/createDate", jsonParser, function(req, res){
+    let id =  uuid4();
+    let date = new Date().toLocaleString('es-MX', {timeZone: 'America/Mexico_City'});
+
+    (async () => {
+        try{
+            let data = {
+                "pk": "S-Schedule",
+                "sk": "Schedule-" + id,
+                "fecha": req.body.fecha,
+                "hora": req.body.hora,
+
+                //Aquí se pondría el sk del paciente
+                "paciente":req.body.paciente,
+
+                //Aquí se pondría el sk del doctor
+                "doctor":req.body.doctor,
+
+                "estadoConsulta": "ACTIVO",
+                "createdAt": date,
+                "changedAt": date
+            };
+
+            var params = {
+                Item: data,
+                ReturnConsumedCapacity: "TOTAL",
+
+                //Nombre de la tabla a la que hará referencia
+                TableName: "medic_schedule_a"
+            };
+
+            dynamodb.put(params, function(err, response){
+                if(err){
+                    res.status(500).send(error);
+                }else{
+                    res.status(200).send(data);
+                }
+            });
+
+        }catch(error){
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+//Obtener lista de citas (para los pacientes)
+app.get("/scheduleList/patient/:paciente", function(req, res){
+    (async ()=> {
+        try{
+            var params = {
+                 //Nombre de la tabla a la que hará referencia
+                TableName: "medic_schedule_a",
+                KeyConditionExpression: "pk = :pk",
+                ExpressionAttributeValues: {
+                    ":pk": "S-Schedule",
+                    ":paciente":req.params.paciente,
+                },
+                ExpressionAttributeNames:{
+                    "#paciente": "paciente"
+                },
+                /*#sk representa la columna de la base de datos
+                  :sk representa lo que mandamos */
+
+                FilterExpression: "#paciente = :paciente"
+            };
+
+            dynamodb.query(params, function(err, response){
+                if(err){
+                    res.status(500).send(err);
+                }else{
+                    res.status(200).send(response.Items);
+                }
+            });
+
+        }catch(error){
+            res.status(500).send(error);
+        }
+    })();
+});
+
+//Obtener lista de citas (para los doctores)
+app.get("/scheduleList/doctor/:doctor", function(req, res){
+    (async ()=> {
+        try{
+            var params = {
+                 //Nombre de la tabla a la que hará referencia
+                TableName: "medic_schedule_a",
+                KeyConditionExpression: "pk = :pk",
+                ExpressionAttributeValues: {
+                    ":pk": "S-Schedule",
+                    ":doctor":req.params.doctor,
+                },
+                ExpressionAttributeNames:{
+                    "#doctor": "doctor"
+                },
+                /*#sk representa la columna de la base de datos
+                  :sk representa lo que mandamos */
+
+                FilterExpression: "#doctor = :doctor"
+            };
+
+            dynamodb.query(params, function(err, response){
+                if(err){
+                    res.status(500).send(err);
+                }else{
+                    res.status(200).send(response.Items);
+                }
+            });
+
+        }catch(error){
+            res.status(500).send(error);
+        }
+    })();
+});
+
+//Obtener información de cita (para pacientes y doctores)
+app.get("/getSchedule/:sk", function(req, res){
+    (async ()=> {
+        try{
+            var params = {
+                 //Nombre de la tabla a la que hará referencia
+                TableName: "medic_schedule_a",
+                KeyConditionExpression: "pk = :pk AND sk = :sk",
+                ExpressionAttributeValues: {
+                    ":pk": "S-Schedule",
+                    ":sk":req.params.sk,
+                }
+            };
+
+            dynamodb.query(params, function(err, response){
+                if(err){
+                    res.status(500).send(err);
+                }else{
+                    res.status(200).send(response.Items);
+                }
+            });
+
+        }catch(error){
+            res.status(500).send(error);
+        }
+    })();
+});
+
+//Cambiar fecha y hora de cita
+app.put("/update/ScheduleDate/:sk", jsonParser, function(req, res){
+    let date = new Date().toLocaleString('es-MX', {timeZone: 'America/Mexico_City'});
+    var params = {
+         //Nombre de la tabla a la que hará referencia
+        TableName: "medic_schedule_a", 
+        Key: {
+            "pk": "Doctor",
+            "sk": req.params.sk
+        },
+        UpdateExpression: "set fecha = :f,"+
+            " hora = :h," + 
+            " changedAt = :chang",
+        ExpressionAttributeValues: {
+            ":f": req.body.fecha,
+            ":h": req.body.hora,
+            ":chang": date
+        },
+        ReturnValues: "UPDATED_NEW"
+    };
+
+    dynamodb.update(params, function(err, response){
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(200).send(response);
+        }
+    });
+});
+
+//Cancelar cita (Esto se puede usar para cambiar el estado de la cita a otros estados, como ASISTIDO, OLVIDADO, etc)
+app.put("/update/ScheduleDate/:sk", jsonParser, function(req, res){
+    let date = new Date().toLocaleString('es-MX', {timeZone: 'America/Mexico_City'});
+    var params = {
+         //Nombre de la tabla a la que hará referencia
+        TableName: "medic_schedule_a", 
+        Key: {
+            "pk": "Doctor",
+            "sk": req.params.sk
+        },
+        UpdateExpression: "set estadoConsulta = :eC," + 
+            "changedAt = :chang",
+        ExpressionAttributeValues: {
+            ":eC": "CANCELADO",
+            ":chang": date
+        },
+        ReturnValues: "UPDATED_NEW"
+    };
+
+    dynamodb.update(params, function(err, response){
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(200).send(response);
+        }
+    });
+});
+
+
+
+
 
 var server = app.listen(4000, function(){
     console.log("Corriendo en puerto 4000");
